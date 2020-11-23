cb802ab5ae9af395a12bd59a01e52534432c908d
petern1953 <petern1953@gmail.com>
2020-11-23 15:30:51 +0100

    alapfájlok (html, js) 1. feladat ok

---
+++ js/feladat01.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Írj egy olyan függvénykifejezést, ami paraméterként vár egy egész számokat tartalmazó tömböt!
+//     (Inputellenőrzést nem kell végezni.)
+// A függvény szorozza meg tömbelemek értékét 1.27-tel, majd pedig térjen vissza az elemek összegével!
+
+const intArray = [1, 2, 3, 4, 5, 6, 7];
+
+const prodSumItems = intArray.map(item => item * 1.27);
+console.log(prodSumItems);
+const prodSumItems2 = intArray.map(item => item * 1.27).reduce((prev, current) => prev + current);
+console.log(prodSumItems2, 'control: 4 * 7 * 1.27 = ', 28 * 1.27);
