bd7c82985b8ae1385b2d52a8223cae32b8429338
petern1953 <petern1953@gmail.com>
2021-05-03 09:24:03 +0200

    1

--- js/feladat01.js
+++ js/feladat01.js
@@ -5,10 +5,14 @@
 // A függvény szorozza meg tömbelemek értékét 1.27-tel, majd pedig térjen vissza az elemek összegével!
 
 const intArray = [1, 2, 3, 4, 5, 6, 7];
+console.log("intArray: ", intArray);
 
 const prodSumItems = intArray.map(item => item * 1.27);
-console.log(prodSumItems);
+console.log("intArray*1.27: ", prodSumItems);
 
 // take 0 as initial value
 const prodSumItems2 = intArray.map(item => item * 1.27).reduce((prev, current) => prev + current, 0);
-console.log(prodSumItems2, 'control: 0 + 4 * 7 * 1.27 = ', 28 * 1.27);
+console.log(
+`prodSumItems: ${prodSumItems2};
+controlSum: 0 + 4 * 7 * 1.27 = ${28 * 1.27}`
+);
